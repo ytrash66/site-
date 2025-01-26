153c03e49d5df50b0dff5c701c43b4976943050f
ytrash66 <140930678+ytrash66@users.noreply.github.com>
2025-01-26 03:46:37 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const animeList = [
     { title: 'Attack on Titan', image: 'https://m.media-amazon.com/images/M/MV5BNjY4MDQxZTItM2JjMi00NjM5LTk0MWYtOTBlNTY2YjBiNmFjXkEyXkFqcGc@._V1_.jpg' },
-    { title: 'Naruto', image: 'https://m.media-amazon.com/images/I/81Zj-BWityL.jpg' },
-    { title: 'One Piece', image: 'https://m.media-amazon.com/images/I/81zwj54srRL.jpg' }
+    { title: 'Naruto', image: 'https://japanesegallery.com/pub/media/catalog/product/cache/7e991fa6b61968a7414c4c2fad2046e8/j/g/jgkp436.jpeg' },
+    { title: 'One Piece', image: 'https://images.squarespace-cdn.com/content/v1/642b13ff2f14ae1372e3b749/1683923997423-S9GD5PBPA0PMQW8R3K1U/One+Piece+.jpg' }
 ];
 
 const animeContainer = document.getElementById('anime-list');
